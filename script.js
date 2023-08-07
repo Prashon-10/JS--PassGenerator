d409e80e829b0424d827e1aae636df1811f65647
Prashon Gautam <48346918+Prashon-10@users.noreply.github.com>
2023-08-06 21:08:43 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+const passwordBox = document.getElementById("Password");
+const length = 12; //Length of the password
+
+const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const lowerCase = "abcdefghijklmnopqrstuvwxyz";
+const Symbols = "!@#$%^&*()_+=-<>?/.';[]{}";
+const numberValue = "0123456789";
+
+const allChars = upperCase + lowerCase + Symbols + numberValue;
+function createPassword() {
+  let password = "";
+
+  password += upperCase[Math.floor(Math.random() * upperCase.length)];
+  password += lowerCase[Math.floor(Math.random() * lowerCase.length)];
+  password += Symbols[Math.floor(Math.random() * Symbols.length)];
+  password += numberValue[Math.floor(Math.random() * numberValue.length)];
+
+  while (length > password.length) {
+    password += allChars[Math.floor(Math.random() * allChars.length)];
+  }
+  passwordBox.value = password;
+}
